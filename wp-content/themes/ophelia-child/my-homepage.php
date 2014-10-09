1e8ba07e9af4d7f21a98dc12f04bcaf109ed7f5e
Sorknes, Knut Arne (OSL-CRA) <knut.sorknes@craftww.com>
2014-10-09 11:18:42 +0200

    Created custom style & homepage

---
+++ wp-content/themes/ophelia-child/my-homepage.php
@@ -0,0 +1,19 @@
+<?php
+/*
+ * Template Name: Startsiden
+ * Description: Custom startpage
+ */
+
+get_header(); 
+
+?>
+
+	<!-- start: content -->
+	<div>
+		<main>
+		
+		</main>
+	</div>
+	<!-- end: content -->
+
+<?php get_footer(); ?>
