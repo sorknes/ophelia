db08308acf20874b31f3a484033e5edaf69cf04d
sorknes <knut.sorknes@gmail.com>
2014-10-11 02:47:54 +0200

    Started layout content

--- wp-content/themes/ophelia-child/my-homepage.php
+++ wp-content/themes/ophelia-child/my-homepage.php
@@ -8,12 +8,45 @@ get_header();
 
 ?>
 
-			<!-- start: content -->
-			<div class="content">
-				<main id="page" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
-				
-				</main>
+			<!-- start: intro -->
+			<div class="intro-outer">
+				<div class="intro-inner">
+					<div class="container-fluid">
+						<div class="row">
+							<div class="col-xs-12">
+								<h1 class="intro-title"><?php bloginfo('name'); ?></h1>
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-xs-12">
+								<div class="title">
+									<h2><?php echo get_bloginfo('description'); ?></h2>
+								</div>
+							</div>
+						</div>
+						<div class="row intro-btn">
+							<div class="col-xs-12 col-sm-6 btn-right">
+								<a class="btn large" href="" title="Bar">
+									Bar
+									<span></span>
+								</a>
+							</div>
+							<div class="col-xs-12 col-sm-6 btn-left">
+								<a class="btn large" href="" title="Dinner">
+									Dinner
+									<span></span>
+								</a>
+							</div>
+						</div>
+					</div>
+				</div>
 			</div>
-			<!-- end: content -->
+			<!-- end: intro -->
+			
+			<div class="container news">
+				<div class="row">
+					news
+				</div>
+			</div>
 
 <?php get_footer(); ?>
