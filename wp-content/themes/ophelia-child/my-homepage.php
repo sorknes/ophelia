4d2c36c10e38e5f38e2ba701427f1f27931c157a
Sorknes, Knut Arne (OSL-CRA) <knut.sorknes@craftww.com>
2014-10-13 14:04:14 +0200

    Added intro post type support

--- wp-content/themes/ophelia-child/my-homepage.php
+++ wp-content/themes/ophelia-child/my-homepage.php
@@ -8,38 +8,60 @@ get_header();
 
 ?>
 
+			<?php // Start Query Intro
+			$args_intro = "post_type=intro&posts_per_page=1&orderby=date&order=DESC";
+				
+			// The Query
+			$query_intro = new WP_Query( $args_intro );
+				
+			// The Loop
+			while ( $query_intro->have_posts() ) {
+				$query_intro->the_post();				
+				$intro_custom_link 		= get_post_custom_values('pt_link');
+				$intro_custom_link 		= $intro_custom_link[0];
+				$intro_custom_link_2	= get_post_custom_values('pt_link_2');
+				$intro_custom_link_2 	= $intro_custom_link_2[0];
+			} ?>
+
 			<!-- start: intro -->
 			<div class="intro-outer">
 				<div class="intro-inner">
+					<!-- start: container-fluid -->
 					<div class="container-fluid">
 						<div class="row">
 							<div class="col-xs-12">
-								<h1 class="intro-title"><?php bloginfo('name'); ?></h1>
+								<h1 class="intro-title"><?php echo get_the_title( $query_intro->post->ID ); ?></h1>
 							</div>
 						</div>
 						<div class="row">
 							<div class="col-xs-12">
-								<div class="title">
-									<h2><?php echo get_bloginfo('description'); ?></h2>
-								</div>
+								<p><?php echo get_the_excerpt( $query_intro->post->ID ); ?></p>
 							</div>
 						</div>
 						<div class="row intro-btn">
 							<div class="col-xs-12 col-sm-6 btn-right">
-								<a class="btn large" href="" title="Bar">
+								<a class="btn large" href="<?php echo $intro_custom_link ?>" title="Bar">
 									Bar
 									<span></span>
 								</a>
 							</div>
 							<div class="col-xs-12 col-sm-6 btn-left">
-								<a class="btn large" href="" title="Dinner">
+								<a class="btn large" href="<?php echo $intro_custom_link_2 ?>" title="Dinner">
 									Dinner
 									<span></span>
 								</a>
 							</div>
 						</div>
 					</div>
+					<!-- end: container-fluid -->
 				</div>
+				<!-- start: hero image -->
+				<div class="intro-img-wrapper">
+			    	<figure id="intro-image">
+			    		<?php echo get_the_post_thumbnail( $query_intro->$page->ID, 'full', array ('class' => '') ); ?>
+					</figure>
+		    	</div>
+				<!-- end: hero image -->
 			</div>
 			<!-- end: intro -->
 			
